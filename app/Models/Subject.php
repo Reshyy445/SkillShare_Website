5eee4668e6b313d012513681384fa9e5e4f7d0d5
Reshyy445 <9026050@student.zadkine.nl>
2026-06-30 17:46:26 +0200

    created profile and subject models

---
+++ app/Models/Subject.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Subject extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'education',
+        'level',
+        'created_by',
+    ];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+}
