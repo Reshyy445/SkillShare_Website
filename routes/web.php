8160f28cd6050403d2b1f800e0fd73491d10fd83
Reshyy445 <9026050@student.zadkine.nl>
2026-06-30 18:33:21 +0200

    created all routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,83 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SubjectController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\MessageController;
+use App\Http\Controllers\AdminController;
 
+// Public routes
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage.index');
+})->name('home');
+
+Route::get('/about', function () {
+    return view('homepage.about');
+})->name('about');
+
+Route::get('/contact', function () {
+    return view('homepage.contact');
+})->name('contact');
+
+Route::get('/news', function () {
+    return view('homepage.news');
+})->name('news');
+
+// Auth routes
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+// Protected routes
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    // Profile routes
+    Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
+    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
+
+    // Subject routes
+    Route::resource('subjects', SubjectController::class)->except(['show']);
+    Route::get('/subjects/{subject}', [SubjectController::class, 'show'])->name('subjects.show');
+
+    // Post routes
+    Route::get('/subjects/{subject}/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::post('/subjects/{subject}/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+
+    // Comment routes
+    Route::post('/posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store');
+    Route::get('/comments/{comment}/edit', [CommentController::class, 'edit'])->name('comments.edit');
+    Route::put('/comments/{comment}', [CommentController::class, 'update'])->name('comments.update');
+    Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
+
+    // Message routes
+    Route::get('/messages', [MessageController::class, 'index'])->name('messages.index');
+    Route::get('/messages/create', [MessageController::class, 'create'])->name('messages.create');
+    Route::post('/messages', [MessageController::class, 'store'])->name('messages.store');
+    Route::get('/messages/{message}', [MessageController::class, 'show'])->name('messages.show');
+    Route::delete('/messages/{message}', [MessageController::class, 'destroy'])->name('messages.destroy');
 });
 
-Route::get('/pages/home', function () {
-    return view('pages.home');
+// Admin routes
+Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+    Route::get('/users', [AdminController::class, 'users'])->name('users');
+    Route::delete('/users/{user}', [AdminController::class, 'destroyUser'])->name('users.destroy');
+    Route::get('/posts', [AdminController::class, 'posts'])->name('posts');
+    Route::delete('/posts/{post}', [AdminController::class, 'destroyPost'])->name('posts.destroy');
+    Route::get('/subjects', [AdminController::class, 'subjects'])->name('subjects');
+    Route::delete('/subjects/{subject}', [AdminController::class, 'destroySubject'])->name('subjects.destroy');
+    Route::get('/comments', [AdminController::class, 'comments'])->name('comments');
+    Route::delete('/comments/{comment}', [AdminController::class, 'destroyComment'])->name('comments.destroy');
 });
