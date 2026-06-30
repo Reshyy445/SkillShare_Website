2dd77e5521ed12b398faf707f7be1c23a5a1c168
Reshyy445 <9026050@student.zadkine.nl>
2026-06-30 17:21:39 +0200

    Initial commit

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
+</head>
+<body>
+
+@include('partials.navbar')
+
+@
+
+@include('partials.footer')
+
+</body>
+</html>
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,40 @@
+<nav class="navbar navbar-expand-lg custom-navbar">
+    <div class="container-fluid">
+
+        <!-- Logo -->
+        <a class="navbar-brand" href="index.html">
+            <img src="images/logo.png" alt="Logo" class="logo">
+        </a>
+
+        <!-- Hamburger menu voor mobiel -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Navigatie -->
+        <div class="collapse navbar-collapse" id="navbarNav">
+
+            <!-- ms-auto duwt de links naar rechts -->
+            <ul class="navbar-nav ms-auto align-items-center">
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">News</a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#" class="btn login-btn">Login</a>
+                </li>
+
+            </ul>
+        </div>
+
+    </div>
+</nav>
